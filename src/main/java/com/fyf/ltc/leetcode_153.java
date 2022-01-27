f9c43eabd2e2f60c3e340eee506094b0167e7816
Fan-Yu-Feng <1905145967@qq.com>
2022-01-27 09:34:03 +0800

    add .gitignore

---
+++ src/main/java/com/fyf/ltc/leetcode_153.java
@@ -0,0 +1,28 @@
+package com.fyf.ltc;
+
+/**
+ * @author FYF @Description
+ * @date 2022/1/26
+ */
+public class leetcode_153 {
+  public static void main(String[] args) {
+    int min = findMin(new int[] {3, 4, 5, 1, 2});
+    System.out.println(min);
+  }
+
+  static int findMin(int[] nums) {
+    int r = nums.length - 1;
+    int l = 0, mid = 0;
+    while (l < r) {
+      mid = l + ((r - l) >> 1);
+      // 最小数在右侧，指针右移
+      if (nums[mid] > nums[r]) {
+        l = mid + 1;
+      } else {
+        // 最小数在左侧,指针左移
+        r = mid;
+      }
+    }
+    return nums[l];
+  }
+}
---
+++ src/main/java/com/fyf/ltc/leetcode_74.java
@@ -0,0 +1,27 @@
+package com.fyf.ltc;
+
+/**
+ * @author FYF @Description
+ * @date 2022/1/26
+ */
+public class leetcode_74 {
+  public static void main(String[] args) {
+    System.out.println(
+        searchMatrix(new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 34, 45, 50}}, 13));
+  }
+
+  public static boolean searchMatrix(int[][] matrix, int target) {
+    if (matrix.length == 1 && matrix[0].length == 1) {
+      return matrix[0][0] == target;
+    }
+    int row = 0, col = matrix[0].length - 1;
+    while (row <= matrix.length - 1 && col >= 0) {
+      if (matrix[row][col] < target) {
+        row++;
+      } else if (matrix[row][col] > target) {
+        col--;
+      } else return true;
+    }
+    return false;
+  }
+}
