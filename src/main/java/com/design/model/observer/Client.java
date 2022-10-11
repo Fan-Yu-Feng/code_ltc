d1e52543bfb3c4ef9d367339e902cbb1ae76fb64
Fan-Yu-Feng <1905145967@qq.com>
2022-10-11 10:44:39 +0800

    chore(observer):观察者模式初始化

---
+++ src/main/java/com/design/model/observer/Client.java
@@ -0,0 +1,31 @@
+package com.design.model.observer;
+
+/**
+ * @author FYF
+ * @version 1.0
+ * @date 2022/10/9 12:52
+ */
+public class Client {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		//定义出韩非子和李斯
+		LiSi liSi = new LiSi();
+		HanFeiZi hanFeiZi = new HanFeiZi();
+		//观察早餐
+		Spy watchBreakfast = new Spy(hanFeiZi,liSi,"breakfast");
+		//开始启动线程，监控
+		watchBreakfast.start();
+		//观察娱乐情况
+		Spy watchFun = new Spy(hanFeiZi,liSi,"fun");
+		watchFun.start();
+		//然后我们看看韩非子在干什么
+		//主线程等待1秒后后再往下执行
+		Thread.sleep(1000);
+		hanFeiZi.haveBreakfast();
+		//韩非子娱乐了
+		Thread.sleep(1000);
+		hanFeiZi.haveFun();
+		
+	}
+}
---
+++ src/main/java/com/design/model/observer/IHanFeiZi.java
@@ -0,0 +1,22 @@
+package com.design.model.observer;
+
+/**
+ * 被观察者接口
+ *
+ * @author FYF
+ * @version 1.0
+ * @date 2022/10/9 12:44
+ */
+public interface IHanFeiZi {
+	
+	/**
+	 * 韩非子也是人，也要吃早饭的
+	 */
+	void haveBreakfast();
+	
+	/**
+	 * 韩非之也是人，是人就要娱乐活动
+	 */
+	void haveFun();
+	
+}
